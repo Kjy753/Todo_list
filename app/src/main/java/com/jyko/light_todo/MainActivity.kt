00b8846d8692f1a8fdf8d3232ce3c507715e4c67
kjy <aty753@naver.com>
2021-04-06 21:19:25 +0900

    데이터 바인딩 이용 리사이클러뷰 로컬 작성

--- app/src/main/java/com/jyko/light_todo/MainActivity.kt
+++ app/src/main/java/com/jyko/light_todo/MainActivity.kt
@@ -2,10 +2,66 @@ package com.jyko.light_todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.jyko.light_todo.databinding.ActivityMainBinding
+import com.jyko.light_todo.databinding.ItemTodoBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // 작업을 위한 데이터 생성
+    private val data = arrayListOf<Todo>()
+
+    // binding 변수 선언
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //binding 에 레이아웃 연결
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //binding에 루트뷰 참조
+        val view = binding.root
+        setContentView(view)
+
+        data.add(Todo("test1",false))
+        data.add(Todo("test2",false))
+
+        // 리사이클러뷰 객체 지정
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = TodoAdapter(data)
+
+
     }
+}
+
+
+class TodoAdapter(private val dataSet: List<Todo>) :
+    RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(var binding: ItemTodoBinding) : RecyclerView.ViewHolder(binding.root) {
+        //view 객체가 아닌 바인딩 객체를 넣어줌.
+
+    }
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): TodoViewHolder {
+
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_todo, viewGroup, false)
+
+        return TodoViewHolder(ItemTodoBinding.bind(view))
+    }
+
+
+    override fun onBindViewHolder(viewHolder: TodoViewHolder, position: Int) {
+
+        viewHolder.binding.todoText.text = dataSet[position].text
+    }
+
+
+    override fun getItemCount() = dataSet.size
+
 }
